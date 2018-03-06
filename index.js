a9f027a8765a0dda97a1e629e374e027af17f097
jacksongreg <gjfpjackson@gmail.com>
2018-03-06 14:14:59 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,41 @@
+
+var favicon = require('serve-favicon');
+var path = require('path');
+var express = require("express");
+var app = express();
+var port = 3000;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://gregjack:1970greg@ds131546.mlab.com:31546/codespace");
+
+var nameSchema = new mongoose.Schema({
+    fname: String,
+    lname: String,
+    email: String
+});
+var User = mongoose.model("Names", nameSchema);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/addname", (req, res) => {
+    var myData = new User(req.body);
+    myData.save()
+        .then(item => {
+            res.send("Name saved to database");
+        })
+        .catch(err => {
+            res.status(400).send("Unable to save to database");
+        });
+});
+
+app.listen(port, () => {});
